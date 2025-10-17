1902fdde0aae3bd371d84f69c7d5e519ab057dab
Apurva Mishra <a0mish06@louisville.edu>
2025-10-16 22:45:17 -0400

    Added HTTP Compression

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from app.routes import story
 
-app = FastAPI()
+app = FastAPI(title="Vocabulary Story API", version="1.0.0")
 
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
